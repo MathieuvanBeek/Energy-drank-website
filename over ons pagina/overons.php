f08dcdeb2773c20f8e34bf0011507017d91e9596
adamchaaibi <74769600+adamchaaibi@users.noreply.github.com>
2021-03-11 11:42:51 +0100

    php toegevoegd

---
+++ over ons pagina/overons.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="language" content="NL">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="A.Chaaibi">
+    <meta name="keywords" content="">
+    <title>Over ons</title>
+    <link rel="stylesheet" type="text/css" href="css/overons.css">
+    <script src="javascript/overons.js" type="text/javascript"></Script> 
+</head>
+<body>
+    <header>
+        <section id="item-logo">
+            <img src="image/vuur logo.png" alt="dit is ons logo" height="100vh" width="100vw">
+        </section>
+    </header>
+
+    <footer>
+    </footer>
+</body>
+</html>
