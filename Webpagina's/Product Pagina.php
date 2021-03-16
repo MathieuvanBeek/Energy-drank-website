c00af115c0378634bf2fc62ced82c62e7332db5c
Mathieu van Beek <6014497@mborijnland.nl>
2021-03-16 13:43:58 +0100

    Header toegevoegt aan product pagina

--- Webpagina's/Product Pagina.php
+++ Webpagina's/Product Pagina.php
@@ -1,18 +1,26 @@
 <!doctype html>
 <html>
-<head>
-<meta charset="UTF-8">
+<meta charset="utf-8">
 <meta http-equiv="language" content="NL">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="website pagina van ons product">
-<meta name="author" content="Diede">
-<meta name="keywords" content="Energy, blikjes, producten"> 
-<title>Product pagina</title>
-<link href="Product pagina.css" rel="stylesheet" type="text/css">
+<meta name="viewport" content="width=device, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="Mathieu van Beek">
+<meta name="keywords" content="">
+<link href="css/product pagina.css" rel="stylesheet" type="text/css">
+<title></title>
 </head>
 <body>
-<p>hoi</p>
-
-
+<header id="item-header">
+        <article class ="item-logo"><img src="img/vuur logo.png" alt="logo"></article>
+        <nav id="item-nav">
+            <ul>
+            <il><a href="#">Home pagina</a></il>
+            <il><a href="#">Producten</a></il>
+            <il><a href="#">Over ons</a></il>
+            <il><a href="#">Contact</a></il>
+            </ul>
+        </nav>
+    </header>
 </body>
+</html>
 
