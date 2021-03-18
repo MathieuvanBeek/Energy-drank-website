a9d9f881aaed7a67a2aca47c5bde288ef790e708
DiedeB <74538290+DiedeB@users.noreply.github.com>
2021-03-18 12:45:26 +0100

    standaart
    de header en footer over genomen

--- Webpagina's/Product Pagina.php
+++ Webpagina's/Product Pagina.php
@@ -3,15 +3,15 @@
 <meta charset="utf-8">
 <meta http-equiv="language" content="NL">
 <meta name="viewport" content="width=device, initial-scale=1">
-<meta name="description" content="Diede">
-<meta name="author" content="">
-<meta name="keywords" content="">
-<link href="css/product pagina.css" rel="stylesheet" type="text/css">
+<meta name="description" content="Energy Product Pagina">
+<meta name="author" content="Diede">
+<meta name="keywords" content="Product Pagina Energy Drink Website">
+<link href="css/Product pagina.css" rel="stylesheet" type="text/css">
 <title></title>
 </head>
-<body>
+<body> 
 <header id="item-header">
-        <article class ="item-logo"><img src="img/vuur logo.png" alt="logo"></article>
+    <article><img src="img/vuur logo.png" alt="logo"></article>
         <nav id="item-nav">
             <ul>
             <il><a href="#">Home pagina</a></il>
@@ -20,7 +20,20 @@
             <il><a href="#">Contact</a></il>
             </ul>
         </nav>
-    </header>
+    <article><img src="img/inlog logo.png" alt="logo"></article>
+    <article><img src="img/search logo.png" alt="logo"></article>
+</header>
+<main>
+</main>
+<footer>
+    <article>
+        <p>Privacybeleid</p>
+        <p>Cookiebeleid</p>
+        <p>Voorwaarden</p>
+        <p>Contact</p>
+        <div>&copy;Bluefire company</div>
+    </article>
+</footer>
 </body>
 </html>
 
