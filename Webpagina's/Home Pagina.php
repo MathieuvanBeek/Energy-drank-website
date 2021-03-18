568ff976153ce1aa922ebcc808370e0d03186384
Mathieu van Beek <6014497@mborijnland.nl>
2021-03-18 12:37:47 +0100

    Home pagina af met header en footer

---
+++ Webpagina's/Home Pagina.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+<meta charset="utf-8">
+<meta http-equiv="language" content="NL">
+<meta name="viewport" content="width=device, initial-scale=1">
+<meta name="description" content="Energy Home Pagina">
+<meta name="author" content="Mathieu van Beek">
+<meta name="keywords" content="Home Pagina Energy Drink Website">
+<link href="css/home pagina.css" rel="stylesheet" type="text/css">
+<title></title>
+</head>
+<body>
+<header id="item-header">
+    <article><img src="img/vuur logo.png" alt="logo"></article>
+        <nav id="item-nav">
+            <ul>
+            <il><a href="#">Home pagina</a></il>
+            <il><a href="#">Producten</a></il>
+            <il><a href="#">Over ons</a></il>
+            <il><a href="#">Contact</a></il>
+            </ul>
+        </nav>
+    <article><img src="img/inlog logo.png" alt="logo"></article>
+    <article><img src="img/search logo.png" alt="logo"></article>
+</header>
+<main>
+</main>
+<footer>
+    <article>
+        <p>Privacybeleid</p>
+        <p>Cookiebeleid</p>
+        <p>Voorwaarden</p>
+        <p>Contact</p>
+        <div>&copy;Bluefire company</div>
+    </article>
+</footer>
+</body>
+</html>
+
