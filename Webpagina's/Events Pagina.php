fd3e1d08930742149357e08f4e3bca65efd445f0
Mathieu van Beek <6014497@mborijnland.nl>
2021-03-23 15:57:30 +0100

    Home pagina main sectie bijgewerkt en events pagina toegevoegd

--- Webpagina's/Events Pagina.php
+++ Webpagina's/Events Pagina.php
@@ -3,10 +3,10 @@
 <meta charset="utf-8">
 <meta http-equiv="language" content="NL">
 <meta name="viewport" content="width=device, initial-scale=1">
-<meta name="description" content="Energy Home Pagina">
+<meta name="description" content="Energy Events Pagina">
 <meta name="author" content="Mathieu van Beek">
-<meta name="keywords" content="Home Pagina Energy Drink Website">
-<link href="css/home pagina.css" rel="stylesheet" type="text/css">
+<meta name="keywords" content="Events Pagina Energy Drink Website">
+<link href="css/events pagina.css" rel="stylesheet" type="text/css">
 <title></title>
 </head>
 <body>
@@ -16,20 +16,15 @@
             <ul>
             <il><a href="Home Pagina.php">Home pagina</a></il>
             <il><a href="Product Pagina.php">Producten</a></il>
-            <il><a href="Overons Pagina.php">Over ons</a></il>
+            <il><a href="Overons pagina.php">Over ons</a></il>
             <il><a href="Events Pagina.php">Events</a></il>
-            <il><a href="Contact Pagina.php">Contact</a></il>
+            <il><a href="Contact pagina.php">Contact</a></il>
             </ul>
         </nav>
     <article><img src="img/inlog logo.png" alt="inlog logo"></article>
     <article><img src="img/search logo.png" alt="search logo"></article>
 </header>
 <main>
-    <section>
-        <article><a href="Events Pagina.php">Events</a></article>
-        <img src="img/blue fire raspberry new flavor.png" alt="raspberry new flavor">
-        <article><a href="Product Pagina.php">Learn more</a></article>
-    </section>
 </main>
 <footer>
     <article>
