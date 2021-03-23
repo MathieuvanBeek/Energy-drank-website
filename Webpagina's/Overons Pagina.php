3dc2130966dbe72270146896766479bcc874166c
gebruiker <6014022@mborijnland.nl>
2021-03-23 12:44:56 +0100

    css veranderd
    css van de over ons pagina veranderd

--- Webpagina's/Overons Pagina.php
+++ Webpagina's/Overons Pagina.php
@@ -1,24 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="language" content="NL">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="A.Chaaibi">
-    <meta name="keywords" content="">
-    <title>Over ons</title>
-    <link rel="stylesheet" type="text/css" href="css/overons.css">
-    <script src="javascript/overons.js" type="text/javascript"></Script> 
+<!doctype html>
+<html>
+<meta charset="utf-8">
+<meta http-equiv="language" content="NL">
+<meta name="viewport" content="width=device, initial-scale=1">
+<meta name="description" content="Energy over ons Pagina">
+<meta name="author" content="A. Chaaibi">
+<meta name="keywords" content="over ons Pagina Energy Drink Website">
+<link href="css/overons.css" rel="stylesheet" type="text/css">
+<title></title>
 </head>
 <body>
-    <header>
-        <section id="item-logo">
-            <img src="img/vuur logo.png" alt="dit is ons logo" height="100vh" width="100vw">
-        </section>
-    </header>
-
-    <footer>
-    </footer>
+<header id="item-header">
+    <article><img src="img/vuur logo.png" alt="logo"></article>
+        <nav id="item-nav">
+            <ul>
+            <il><a href="Home Pagina.php">Home pagina</a></il>
+            <il><a href="Product Pagina.php">Producten</a></il>
+            <il><a href="Overons pagina.php">Over ons</a></il>
+            <il><a href="#">Contact</a></il>
+            </ul>
+        </nav>
+    <article><img src="img/inlog logo.png" alt="inlog logo"></article>
+    <article><img src="img/search logo.png" alt="search logo"></article>
+</header>
+<main>
+</main>
+<footer>
+    <article>
+        <p>Privacybeleid</p>
+        <p>Cookiebeleid</p>
+        <p>Voorwaarden</p>
+        <p>Contact</p>
+        <div>&copy;Bluefire company</div>
+    </article>
+</footer>
 </body>
 </html>
+
