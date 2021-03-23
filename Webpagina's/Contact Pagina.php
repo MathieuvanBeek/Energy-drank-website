347efbe354d138863aa4994c10574003fbe7b26d
ayyou <6014074@mborijnland.nl>
2021-03-23 13:08:29 +0100

    contact toegevoegd

---
+++ Webpagina's/Contact Pagina.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+<meta charset="utf-8">
+<meta http-equiv="language" content="NL">
+<meta name="viewport" content="width=device, initial-scale=1">
+<meta name="description" content="Energy Contact Pagina">
+<meta name="author" content="Ayyoub Benhajar">
+<meta name="keywords" content="Contact Pagina Energy Drink Website">
+<link href="css/contact pagina.css" rel="stylesheet" type="text/css">
+<title></title>
+</head>
+<body>
+<header id="item-header">
+    <article><img src="img/vuur logo.png" alt="logo"></article>
+        <nav id="item-nav">
+            <ul>
+            <il><a href="Home Pagina.php">Home pagina</a></il>
+            <il><a href="Product Pagina.php">Producten</a></il>
+            <il><a href="Overons pagina. php">Over ons</a></il>
+            <il><a href="#">Contact</a></il>
+            </ul>
+        </nav>
+    <article><img src="img/inlog logo.png" alt="inlog logo"></article>
+    <article><img src="img/search logo.png" alt="search logo"></article>
+</header>
+<main>
+</main>
+<footer>
+    <article>
+        <p>Privacybeleid</p>
+        <p>Cookiebeleid</p>
+        <p>Voorwaarden</p>
+        <p>Contact</p>
+        <div>&copy;Bluefire company</div>
+    </article>
+</footer>
+</body>
+</html>
+
