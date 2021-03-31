a9badd253819d0e31dc8e0f5a42307225d9b3187
ayyou <6014074@mborijnland.nl>
2021-03-31 23:29:20 +0200

    contact pagina

--- Webpagina's/Contact Pagina.php
+++ Webpagina's/Contact Pagina.php
@@ -1,39 +1,66 @@
 <!doctype html>
 <html>
-<meta charset="utf-8">
-<meta http-equiv="language" content="NL">
-<meta name="viewport" content="width=device, initial-scale=1">
-<meta name="description" content="Energy Contact Pagina">
-<meta name="author" content="Ayyoub Benhajar">
-<meta name="keywords" content="Contact Pagina Energy Drink Website">
-<link href="css/contact pagina.css" rel="stylesheet" type="text/css">
-<title></title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="language" content="NL">
+    <meta name="viewport" content="width=device, initial-scale=1">
+    <meta name="description" content="Energy Contact Pagina">
+    <meta name="author" content="Ayyoub Benhajar">
+    <meta name="keywords" content="Contact Pagina Energy Drink Website">
+    <link href="css/contact pagina.css" rel="stylesheet" type="text/css">
+    <script src="js/main script.js" type="text/javascript"></script>
+    <title>contact</title>
 </head>
 <body>
-<header id="item-header">
-    <article><img src="img/vuur logo.png" alt="logo"></article>
-        <nav id="item-nav">
-            <ul>
-            <il><a href="Home Pagina.php">Home pagina</a></il>
-            <il><a href="Product Pagina.php">Producten</a></il>
-            <il><a href="Overons pagina.php">Over ons</a></il>
-            <il><a href="Contact pagina.php">Contact</a></il>
-            </ul>
-        </nav>
-    <article><img src="img/inlog logo.png" alt="inlog logo"></article>
-    <article><img src="img/search logo.png" alt="search logo"></article>
-</header>
-<main>
-</main>
-<footer>
-    <article>
-        <p>Privacybeleid</p>
-        <p>Cookiebeleid</p>
-        <p>Voorwaarden</p>
-        <p>Contact</p>
-        <div>&copy;Bluefire company</div>
-    </article>
-</footer>
+    <header id="item-header">
+        <article><img src="img/vuur logo.png" alt="logo"></article>
+            <nav id="item-nav">
+                <ul>
+                <il><a href="Home Pagina.php">Home pagina</a></il>
+                <il><a href="Product Pagina.php">Producten</a></il>
+                <il><a href="Overons pagina.php">Over ons</a></il>
+                <il><a href="Contact pagina.php">Contact</a></il>
+                </ul>
+            </nav>
+        <article><img src="img/inlog logo.png" alt="inlog logo"></article>
+        <article><img src="img/search logo.png" alt="search logo"></article>
+        <p id="drop-nav">&#9776;</p>
+            <nav id="menu">
+                    <a href="Home Pagina.php">Home pagina</a>
+                    <a href="Product Pagina.php">Producten</a>
+                    <a href="Overons pagina.php">Over ons</a>
+                    <a href="Events Pagina.php">Events</a>
+                    <a href="Contact pagina.php">Contact</a>
+            </nav>
+    </header>
+    <main>
+        <form action="#" method="post" id="form">
+            <div class="group">
+                <input type="text" class="input" id="naam" placeholder="Naam">
+            </div>
+            <div class="group">
+                <input type="email" class="input" id="email" placeholder="Email">
+            </div>
+            <div class="group">
+                <input type="text" class="input" id="onderwerp" placeholder="Onderwerp">
+            </div>
+            <div class="group">
+                <textarea name="" id="text_area" placeholder="Vragen of opmerkingen"></textarea>
+            </div>
+            <div>
+                <button type="submit" id="button">Versturen</button>
+            </div>
+        </form>
+    </main>
+    <footer>
+        <article>
+            <p>Privacybeleid</p>
+            <p>Cookiebeleid</p>
+            <p>Voorwaarden</p>
+            <p>Contact</p>
+            <div>&copy;Bluefire company</div>
+        </article>
+    </footer>
 </body>
 </html>
 
