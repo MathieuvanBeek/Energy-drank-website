ed5048e501e9abc85c23b94d34978d34a9f2b5a3
ayyou <6014074@mborijnland.nl>
2021-04-08 09:12:24 +0200

    Ik heb de eventspagina toegevoegd aan het menu

--- Webpagina's/Contact Pagina.php
+++ Webpagina's/Contact Pagina.php
@@ -19,6 +19,7 @@
                 <il><a href="Home Pagina.php">Home pagina</a></il>
                 <il><a href="Product Pagina.php">Producten</a></il>
                 <il><a href="Overons pagina.php">Over ons</a></il>
+                <il><a href="Events Pagina.php">Events</a></il>
                 <il><a href="Contact pagina.php">Contact</a></il>
                 </ul>
             </nav>
