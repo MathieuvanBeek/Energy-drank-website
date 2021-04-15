42cb3f05fa2267c2d776ac8079f6fe08af6be78b
DiedeB <74538290+DiedeB@users.noreply.github.com>
2021-04-15 11:32:19 +0200

    Update Product energy.js

--- Webpagina's/Product energy.js
+++ Webpagina's/Product energy.js
@@ -1,15 +1,14 @@
 var test;
 var index = 0;
-
 var slideShowArray = ['normal Blue fire','green apple Blue fire','grape Blue fire','rasberry Blue fire'];
 
 
 
 
-window.onload = fuction()
+window.onload = fuction(){
     test = document.getElementById("test");
    setInterval(slideShow, 3500);
-
+}
 
 
 function slideShowArray(){
