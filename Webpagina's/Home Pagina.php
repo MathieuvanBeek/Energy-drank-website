a76400b9c0f812f807ac592806a1530a1ef731cd
ayyou <6014074@mborijnland.nl>
2021-04-15 10:42:02 +0200

    menu verandert 2.0

--- Webpagina's/Home Pagina.php
+++ Webpagina's/Home Pagina.php
@@ -18,8 +18,9 @@
             <il><a href="Home Pagina.php">Home pagina</a></il>
             <il><a href="Product Pagina.php">Producten</a></il>
             <il><a href="Overons Pagina.php">Over ons</a></il>
-            <il><a href="Contact Pagina.php">Contact</a></il>
             <il><a href="Events Pagina.php">Events</a></il>
+            <il><a href="Contact Pagina.php">Contact</a></il>
+            
             
             </ul>
         </nav>
